b117022d6857421838954f5fc14dad35102cae87
xeri <xerioo@gmail.com>
2024-04-28 18:40:36 +0200

    Minden funkció működik.

--- src/main/Csigaverseny.java
+++ src/main/Csigaverseny.java
@@ -1,5 +1,6 @@
 package main;
 
+import static java.lang.Integer.parseInt;
 import main.Csiga;
 
 public class Csigaverseny {
@@ -10,20 +11,29 @@ public class Csigaverseny {
     public static void main(String[] args) {
 
         indulas();
-        //fogadas();
+        int tipp = fogadas();
         for (int aktualisKor = 1; aktualisKor < 6; aktualisKor++) {
-            versenytRajzol(aktualisKor);
             for (int i = 0; i < 3; i++) {
-                csiga[i].setPozicio(csiga[i].getPozicio()+csiga[i].getSebesseg());
                 csiga[i].setSebesseg(Csiga.rnd.nextInt(4));
+                csiga[i].setPozicio(csiga[i].getPozicio()+csiga[i].getSebesseg());
             }
             if (Csiga.rnd.nextInt(100) <= turboEsely) {
                 int kivalasztott = Csiga.rnd.nextInt(3);
                 csiga[kivalasztott].setSebesseg(csiga[kivalasztott].getSebesseg()*2);
             }
-            try{System.in.read();}
-            catch(Exception e){}
-         }
+            versenytRajzol(aktualisKor);
+            if (aktualisKor<5) { 
+                System.out.println("Nagyon izgalmas a verseny, a kovetkezo kort az Enterrel indithatod.");
+                try{System.in.read();}
+                catch(Exception e){}
+            
+            }
+        }
+        int gyoztes = kiNyert();
+        System.out.println("Fantasztikus hajraval vegul gyozott a " + csiga[gyoztes-1].getSzin() + " csiga.");
+        if (tipp==gyoztes) {
+            System.out.println("Gratulalok, eltalaltad!");
+        } else System.out.println("Nem jott be a tipped.");
 
     }
      public static void indulas() {
@@ -38,8 +48,11 @@ public class Csigaverseny {
         }
     }
 
-    private static void fogadas() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    private static int fogadas() {
+        versenytRajzol(0);
+        System.out.println("Itt allunk a rajtvonalnal, ki lesz a gyoztes? ( 1 / 2 / 3 )");
+        String tippelt = System.console().readLine();
+        return parseInt(tippelt);
     }
     
     public static void versenytRajzol(int kor){
@@ -48,8 +61,8 @@ public class Csigaverseny {
         for (int i = 0; i < 3; i++) {
             csigatRajzol(csiga[i]);
         }
-            
-        }
+    }
+    
     public static void csigatRajzol(Csiga jocsiga){
         String vonalSzin = "";
         switch (jocsiga.getSzin()) {
@@ -62,6 +75,16 @@ public class Csigaverseny {
             System.out.print("_");
         }
         System.out.println("@");
+    }
+
+    private static int kiNyert() {
+        int nyeresreAll = 0;
+        for (int i = 1; i < 3; i++) {
+            if (csiga[i].getPozicio()>csiga[nyeresreAll].getPozicio()) {
+                nyeresreAll = i;
+            }
+        }
+        return nyeresreAll+1;
     }
     
 }
